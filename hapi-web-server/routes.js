3a5ace7f7e5156dd11b852fbefcd1b5332c7eddf
flowarth <ranggasetiawan242@gmail.com>
2023-05-16 07:26:35 +0700

    belajar membuat web server dengan framework

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,39 @@
+const routes = [
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            const { lang } = request.query;
+
+            if(lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+            
+            return `Hello, ${name}!`;
+        },
+    },
+];
+ 
+module.exports = routes;
